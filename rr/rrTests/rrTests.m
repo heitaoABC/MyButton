e4a4f276a9258ac58bc030930fc3066e80fdcf7e
JuLing Tom <juling_tom@juling-tomdemacbook-pro.local>
2016-07-01 17:12:09 +0800

    初始化

---
+++ rr/rrTests/rrTests.m
@@ -0,0 +1,39 @@
+//
+//  rrTests.m
+//  rrTests
+//
+//  Created by JuLing_Tom on 16/7/1.
+//  Copyright © 2016年 JuLing_Tom. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface rrTests : XCTestCase
+
+@end
+
+@implementation rrTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
